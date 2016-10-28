34ca18a81c5eb15bfca89ef0bfd0d5200a26b579
Bruce Fraser <brucefraser@Bruces-iMac.local>
2016-10-28 17:13:20 +1300

    Make seed fixed and add the jupyter notebook for playing with the scripts

--- neural-networks-and-deep-learning/src/stochastic_gradient_descent_30_100_3.py
+++ neural-networks-and-deep-learning/src/stochastic_gradient_descent_30_100_3.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+
+# Set the seed to make changes deterministic
+import random
+random.seed(1)
+
+import numpy as np
+np.random.seed(1)
+
 import mnist_loader
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
